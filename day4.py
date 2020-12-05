bde576febee7c40cc07601686ada9f9d57554927
jpribeir <joaovlribeiro@msn.com>
2020-12-05 13:02:55 +0000

    Added part2 with comments

--- day4.py
+++ day4.py
@@ -1,3 +1,5 @@
+import re
+
 # class to instantiate travelers' passaports
 class Passenger:
     def __init__(self,info):
@@ -10,6 +12,9 @@ class Passenger:
         # set passport info from input
         self.set_passport(info)
 
+    def aaa(self):
+        return
+
     # set info
     def set_passport(self,info):
         for each in info:
@@ -18,11 +23,28 @@ class Passenger:
     # check each info field, set passport and northpole status, and return only northpole status
     def check_passport(self):
         for each in self.infolist:
-            if not getattr(self,each): break
+            if not getattr(self,each): return False
         else:
+            if not self.validate_year(self.byr,4,1920,2002): return False   #byr
+            if not self.validate_year(self.iyr,4,2010,2020): return False   #iyr
+            if not self.validate_year(self.eyr,4,2020,2030): return False   #eyr
+            if (self.hgt).endswith("cm"):   #hgt
+                if not self.validate_year((self.hgt).split("cm")[0],3,150,193): return False
+            elif (self.hgt).endswith("in"):
+                if not self.validate_year((self.hgt).split("in")[0],2,59,76): return False
+            else: return False
+            if not (re.search("\#[0-9a-f]{6}",self.hcl)): return False  #hcl
+            if self.ecl not in ("amb","blu","brn","gry","grn","hzl","oth"): return False    #ecl
+            if not (len(self.pid)==9 and (self.pid).isnumeric()): return False    #pid
+            
+            # if no previous return was called, nortpole is valid
             self.northpole_valid = True
-            if self.cid: self.passport_valid = True
-        return self.northpole_valid
+            if self.cid: self.passport_valid = True #cid
+            return self.northpole_valid
+
+    def validate_year(self,field,l,min,max):
+        if len(field)==l and int(field)>=min and int(field)<=max: return True
+        else: return False
 
 # open input file and go thorugh each passport and register into objects
 with open("passports.txt","r") as passports:
@@ -41,5 +63,4 @@ with open("passports.txt","r") as passports:
         if newPassenger.check_passport(): count_valid +=1
 
 # output answers
-print("*----- FIRST PART -----*\nThere are %s valid passports\n\n" % count_valid)
-print("*----- SECOND PART -----*\n")
+print("There are %s valid passports\n\n" % count_valid)
