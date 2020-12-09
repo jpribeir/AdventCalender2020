b88a81280ef1d5a1985b456b6e60f9a1efa995b0
jpribeir <joaovlribeiro@msn.com>
2020-12-09 23:22:31 +0000

    Added part 2 with comments

--- code/day9.py
+++ code/day9.py
@@ -15,7 +15,33 @@ for num,line in enumerate(data_list[preamble:-1]):
         if pairFound: break
     if not pairFound:
         first_weakness = line
+        weakness_line = num+preamble
         break
 
+# reverse list so it can start looking from the last line before weakness line
+exploit_list = data_list[0:weakness_line-1]
+exploit_list.reverse()
+weakFound = False
+# set starting point in outmost loop, set end point in next loop
+for num,start in enumerate(exploit_list):
+    for ber,end in enumerate(exploit_list[num+1:-1]):
+        sumall = 0
+        # innermost loop adds all numbers between the 2 points
+        for each in range(num,num+ber+1): sumall += int(exploit_list[each])
+        if sumall == int(first_weakness):
+            minweak = int(first_weakness)
+            maxweak = 0
+            weakFound = True
+            for each in range(num,num+ber+1):
+                if int(exploit_list[each]) < minweak: minweak = int(exploit_list[each])
+                if int(exploit_list[each]) > maxweak: maxweak = int(exploit_list[each])
+                enc_weak = minweak + maxweak
+            break
+        # if sum is bigger than weakness, move start point up
+        # else continue moving end point up
+        elif sumall > int(first_weakness): break
+    if weakFound: break
+
 # output answers
-print("*----- FIRST PART -----*\nThe first weakness in the code is %s in line %s\n" % (first_weakness,num+preamble+1 ))
+print("*----- FIRST PART -----*\nThe first weakness in the code is %s in line %s\n" % (first_weakness,weakness_line+1))
+print("*----- SECOND PART -----*\nThe encryption weakness in the code is %s\n" % enc_weak)
