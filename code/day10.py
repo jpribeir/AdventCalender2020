44214fc12143886199277e859a81d33948a7a91d
jpribeir <joaovlribeiro@msn.com>
2020-12-10 18:57:47 +0000

    Added part 2 with comments

--- code/day10.py
+++ code/day10.py
@@ -1,3 +1,16 @@
+# check if it's the last node; else check if it's a known node; else go through all branches and store returned count in memory
+def count_paths(node,adapter_list,visited):
+    if node == (len(adapter_list)-1): return 1
+    if adapter_list[node] in visited: return visited[adapter_list[node]]
+    subtree_count = 0
+    for index in range(1,4):
+        if node+index < len(adapter_list):
+            if adapter_list[node+index] <= (adapter_list[node]+3): subtree_count += count_paths(node+index,adapter_list,visited)
+            else: break
+        else: break
+    visited[adapter_list[node]] = subtree_count
+    return subtree_count
+
 # open input file and list adapters
 with open("../input_files/jolt_adapters.txt","r") as jolt_adapters:
     adapter_list = []
@@ -15,5 +28,9 @@ for adp in adapter_list:
     else: print("%s is not compatible with previous adapter"%adp)
     prev_adp = adp
 
+# recursion gos through whole tree of paths
+num_paths = count_paths(0,[0]+adapter_list,{})
+
 # output answers
 print("*----- FIRST PART -----*\nThere are %s 1-jolt differences and %s 3-jolt differences,\nwhich multiplied is %s\n"%(rating_diffs[1], rating_diffs[3], rating_diffs[1]*rating_diffs[3]))
+print("*----- SECOND PART -----*\nThere are %s possible combinations" % num_paths)
