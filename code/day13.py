b6913f2cc40a4906903b979446bc39921422bb13
jpribeir <joaovlribeiro@msn.com>
2020-12-14 23:31:38 +0000

    Part 2 added with comments

--- code/day13.py
+++ code/day13.py
@@ -1,10 +1,21 @@
+# recursion
+def find_time(bus_list,count,min_den,current_time):
+    count +=1
+    if count == len(bus_list): return current_time   #if end just return
+    if bus_list[count] == "x": return find_time(bus_list,count,min_den,current_time)    #if "x" continue to next bus ID
+    else:
+        while True: #keep adding up the time until there is a common denominator time instant between the previous IDs and the current one
+            if (current_time + count) % int(bus_list[count]) == 0: return find_time(bus_list,count,min_den*int(bus_list[count]),current_time)
+            current_time +=min_den
+
 # open input file and loop through instructions
 with open("../input_files/bus_info.txt","r") as bus_info:
     info_list = bus_info.readlines()
     arrival = int((info_list[0]).strip())
     earliest = arrival*2
+    bus_list = []
     for each in (info_list[1]).split(","):
-        #if each !="x": bus_schedule.append(each)
+        bus_list.append(each)
         if each == "x": continue
         t = int(each)
         while t < arrival: t +=int(each)
@@ -12,4 +23,8 @@ with open("../input_files/bus_info.txt","r") as bus_info:
             earliest = t
             chosen = int(each)
 
+# find timestamp
+timestamp = find_time(bus_list,0,int(bus_list[0]),int(bus_list[0]))
+
 print("*----- FIRST PART -----*\nThe answer is %s\n" % ((earliest-arrival)*int(chosen)))
+print("*----- SECOND PART -----*\nThe answer is %s\n" % timestamp)
